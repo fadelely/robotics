1104bb6b331717dfc834704b59a4cbd27f70e076
0rahma0 <03rahma.ahmed@gmail.com>
2025-11-23 19:24:57 +0200

    calculate c2 adn c3 by solvinf simeltaneous equations adn print them at end before q and q dot values

--- Milestone 04/joint_space_trajectory.py
+++ Milestone 04/joint_space_trajectory.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sympy as sp
 
 # Initial joints 
 Initial_Q = [-2.788547563050074, -0.5807988920173397, 1.102026594222845, 
@@ -14,8 +15,26 @@ t = 10
 # q(t) = c0 + c1*t + c2*t^2 + c3*t^3 , we used third degree polynomial
 C0 = Initial_Q # Starting position
 C1 = [0, 0, 0, 0, 0, 0, 0]  # initial velocity is 0s
-C2 = [0, 0, 0, 0, 0, 0, 0]  # initial acceleration is 0s
-C3 = [(Final_Q[i] - C0[i]) / (t**3) for i in range(7)]  # calculate c3 for each joint
+# C2 = [0, 0, 0, 0, 0, 0, 0]  # initial acceleration is 0s
+# C3 = [(Final_Q[i] - C0[i]) / (t**3) for i in range(7)]  # calculate c3 for each joint
+
+C2 = []
+C3 = []
+    
+from sympy import symbols, Eq, solve
+
+for joint_index in range(7):
+    c2_tmp, c3_tmp = symbols('c2_tmp c3_tmp')
+    # q equation
+    eq_1 = Eq(C0[joint_index] + c2_tmp*(t)**2 + c3_tmp*(t)**3 - Final_Q[joint_index]) 
+    # q_dot equation
+    eq_2 = Eq(2*c2_tmp*(t) + 3*c3_tmp*(t)**2 - 0)
+    #solve
+    solved_eq = solve((eq_1, eq_2), (c2_tmp, c3_tmp))
+    # append to C2 and C3 lists
+    C2.append(solved_eq[c2_tmp])
+    C3.append(solved_eq[c3_tmp])
+
 
 # Time steps from 0 to 10 with step size 1
 time_steps = np.arange(0, 11, 1)
@@ -23,13 +42,14 @@ time_steps = np.arange(0, 11, 1)
 # Initialize list 0f equations 
 q_traj = []              # q(t) = c0 + c1*t + c2*t^2 + c3*t^3
 q_dot_traj = []          # q_dot(t) = c1 + 2*c2*t + 3*c3*t^2
-q_double_dot_traj = []   # q_double_dot(t) = 2*c2 + 6*c3*t
+# q_double_dot_traj = []   # q_double_dot(t) = 2*c2 + 6*c3*t
+
 
 # Calculate equations for all 7 joints
 for joint_index in range(7):
     q_values = [] # Position values
     q_dot_values = [] # Velocity values
-    q_double_dot_values = [] # Acceleration values
+    # q_double_dot_values = [] # Acceleration values
     
     for t_curr in time_steps:
         # Position equation
@@ -46,18 +66,21 @@ for joint_index in range(7):
         # same here, [q1_dot,q2_dot,...q6_dot] at t=0, then at t=1 ..etc
         q_dot_values.append(q_dot_t)
         # same here, [q1_double_dot,q2_double_dot,...q6_double_dot] at t=0, then at t=1 ..etc
-        q_double_dot_values.append(q_double_dot_t)
+        # q_double_dot_values.append(q_double_dot_t)
     
     # then, we add the list of each joint to the main list so it will look like [[q0 .. q6 at t=0], [q0 .. q6 at t=1] ..etc]
     q_traj.append(q_values)
     # same here , [[q1_dot .. q6_dot at t=0], [q1_dot .. q6_dot at t=1] ..etc]
     q_dot_traj.append(q_dot_values)
     # same here , [[q1_double_dot .. q6_double_dot at t=0], [q1_double_dot .. q6_double_dot at t=1] ..etc]
-    q_double_dot_traj.append(q_double_dot_values)
+    # q_double_dot_traj.append(q_double_dot_values)
 
 # Print results for each joint
+print("  q_c2:", [round(x, 2) for x in C2])
+print("  q_c3:", [round(x, 2) for x in C3])
+
 for joint_index in range(7):
     print(f"\nJoint {joint_index}:")
     print(f"  q(t):            {[round(val, 2) for val in q_traj[joint_index]]}")
     print(f"  q_dot(t):        {[round(val, 2) for val in q_dot_traj[joint_index]]}")
-    print(f"  q_double_dot(t): {[round(val, 2) for val in q_double_dot_traj[joint_index]]}")
+    # print(f"  q_double_dot(t): {[round(val, 2) for val in q_double_dot_traj[joint_index]]}")
